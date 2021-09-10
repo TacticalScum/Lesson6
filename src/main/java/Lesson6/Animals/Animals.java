7d2a78ee50ec2d700db358a053617694edd0cf62
TacticalScum < ,    >
2021-09-10 22:18:22 +0700

    another home work

--- src/main/java/Lesson6/Animals/Animals.java
+++ src/main/java/Lesson6/Animals/Animals.java
@@ -1,21 +1,21 @@
 package Lesson6.Animals;
 
-    public class Animals {
+public class Animals {
 
-        protected String name;
-        protected int animalRun;
-        protected int animalSwim;
+    protected String name;
+    protected int animalRun;
+    protected int animalSwim;
 
-        public Animals(String name, int animalRun, int animalSwim) {
-            this.name = name;
-            this.animalRun = animalRun;
-            this.animalSwim = animalSwim;
-        }
-
-        public void animalRun() {
-        }
-
-        public void animalSwim() {
-        }
+    public Animals(String name, int animalRun, int animalSwim) {
+        this.name = name;
+        this.animalRun = animalRun;
+        this.animalSwim = animalSwim;
     }
+
+    public void animalRun() {
+    }
+
+    public void animalSwim() {
+    }
+}
 
